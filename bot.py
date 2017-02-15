4f9b0e8c4d28f4c6bfb0bbd3c7ad8598b9506027
Federico Rapetti <fuji1097@gmail.com>
2017-02-15 22:41:03 +0100

    Os imported to bot.py

--- bot.py
+++ bot.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with python-telegram-bot-openshift.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
 import logging
 from queue import Queue
 from threading import Thread
